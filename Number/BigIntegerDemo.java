375b137ac802cc755a167311198fe5a3ba793fc8
Askia <56645685+ArgentoAskia@users.noreply.github.com>
2022-07-05 18:07:43 +0800

    数字常用类API-Demo
    包括包装器类、BigDecimal、BigInteger、Random和Math类的常用方法

---
+++ Number/BigIntegerDemo.java
@@ -0,0 +1,12 @@
+package Java08.Number;
+
+import java.math.BigInteger;
+
+public class BigIntegerDemo {
+    public static void main(String[] args) {
+        // BigInteger
+        // radix:进制位
+        BigInteger bigInteger = new BigInteger("999999999999999999999999999");
+
+    }
+}
