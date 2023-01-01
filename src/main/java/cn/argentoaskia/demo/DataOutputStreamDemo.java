a1ed1176041a3a3951c62e21cefb110319e46d22
ArgentoAskia <askiaxiaoming@outlook.com>
2023-01-02 00:36:08 +0800

    修改DataOutputStream路劲

--- src/main/java/cn/argentoaskia/demo/DataOutputStreamDemo.java
+++ src/main/java/cn/argentoaskia/demo/DataOutputStreamDemo.java
@@ -1,6 +1,7 @@
 package cn.argentoaskia.demo;
 
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -10,7 +11,15 @@ import java.io.IOException;
 public class DataOutputStreamDemo {
 //    生成数据
     public static void main(String[] args) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream("D:\\OpenSourceProject\\JavaProject\\Java-IOStream\\src\\main\\resources\\DataFile.txt");
+        // 先描述一下路劲问题，比方说我们当前的大项目是JavaProject（D:\OpenSourceProject\JavaProject），
+        // 在JavaProject里面有很多个模块如Java-Exception（D:\OpenSourceProject\JavaProject\Java-Exception）、Java-IOStream（D:\OpenSourceProject\JavaProject\Java-IOStream）等等
+        // 则直接new File("DataFile.txt")，这个文件会被放在项目根目录，变成（D:\OpenSourceProject\JavaProject\DataFile.txt）
+        // 如果是这样写new File("/DataFile.txt")，则是要求放在根目录的意思，会变成：D:\DataFile.txt
+        // 因此需要手动target等等
+//        File file = new File("Java-IOStream/src/main/resources/DataFile.txt");
+//        String path = file.getAbsolutePath();
+//        System.out.println(path);
+        FileOutputStream fileOutputStream = new FileOutputStream("Java-IOStream/src/main/resources/DataFile.txt");
         DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
         dataOutputStream.write("Hello World!".getBytes());
         dataOutputStream.writeBoolean(true);
