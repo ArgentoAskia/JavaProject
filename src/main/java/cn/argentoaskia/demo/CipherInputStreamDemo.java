ff685ead73b14f89825ce56993ac5bed17d544e1
ArgentoAskia <askiaxiaoming@outlook.com>
2023-01-02 02:35:53 +0800

    CiperintputStreamDemo

--- src/main/java/cn/argentoaskia/demo/CipherInputStreamDemo.java
+++ src/main/java/cn/argentoaskia/demo/CipherInputStreamDemo.java
@@ -3,6 +3,7 @@ package cn.argentoaskia.demo;
 import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -21,6 +22,14 @@ public class CipherInputStreamDemo {
     private static final String ENCRYPT_KEY = "mQbJILokBccRHUkS+XBk7A==";
 
     public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IOException {
+       // 0.加密之前的内容：
+        InputStream dataInputStream0 = CipherOutputStream.class.getResourceAsStream("/data.txt");
+        byte[] bytesData = new byte[dataInputStream0.available()];
+        dataInputStream0.read(bytesData);
+        System.out.println("加密之前的内容：" + Arrays.toString(bytesData) + new String(bytesData, StandardCharsets.UTF_8));
+        dataInputStream0.close();
+
+
         // 1.将内容创建成流
         InputStream dataInputStream = CipherOutputStream.class.getResourceAsStream("/data.txt");
 
@@ -28,7 +37,7 @@ public class CipherInputStreamDemo {
         Cipher cipher = Cipher.getInstance(ENCRYPT_TYPE);
         // 创建加密密钥
         Key key = new SecretKeySpec(ENCRYPT_KEY.getBytes(), ENCRYPT_TYPE);
-        // 初始化加密算法
+        // 初始化加密算法，注意加密的话要指定ENCRYPT_MODE常量
         cipher.init(Cipher.ENCRYPT_MODE, key);
 
         // 3.创建加密流
@@ -46,10 +55,9 @@ public class CipherInputStreamDemo {
             totalRead = totalRead + read;
             byte[] bytes = Arrays.copyOf(encryptBytes, read);
             list.add(bytes);
-            System.out.println(Arrays.toString(bytes));
-            System.out.println(new String(bytes));
         }
         byte[] totalBytes = new byte[totalRead];
+        // 将List中所有的字节复制刀totalBytes
         int cursor = list.get(0).length;
         for (int i = 0; i < list.size(); i++) {
             if (i == 1){
@@ -65,7 +73,18 @@ public class CipherInputStreamDemo {
                 totalBytes[a++] = b;
             }
         }
-        System.out.println(Arrays.toString(totalBytes));
+        System.out.println("加密后的文件字节：" + Arrays.toString(totalBytes) + new String(totalBytes, StandardCharsets.UTF_8));
+
+        // 4.将加密后的文件写出去用于解密
+        File file = new File("Java-IOStream/src/main/resources/data-cipher.txt");
+        if (!file.exists()){
+            file.createNewFile();
+        }
+        FileOutputStream cipherFile = new FileOutputStream(file);
+        cipherFile.write(totalBytes);
+        System.out.println("已将加密内容写到data-cipher.txt文件中，请运行CipherOutputStreamDemo来进行解密");
+
+        cipherFile.close();
         cipherInputStream.close();
         dataInputStream.close();
     }
