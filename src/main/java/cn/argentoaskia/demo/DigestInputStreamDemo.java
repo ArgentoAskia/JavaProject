2ccb96c674d9b8f6df5cfd39a905c801c51637fd
ArgentoAskia <askiaxiaoming@outlook.com>
2023-01-17 00:37:58 +0800

    重写DigestInputStreamDemo和DigestOutputStreamDemo，模拟文件校验过程

--- src/main/java/cn/argentoaskia/demo/DigestInputStreamDemo.java
+++ src/main/java/cn/argentoaskia/demo/DigestInputStreamDemo.java
@@ -1,10 +1,10 @@
 package cn.argentoaskia.demo;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.DigestInputStream;
-import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -18,33 +18,61 @@ import java.util.Arrays;
  */
 public class DigestInputStreamDemo {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        // 你可以修改一下Data.txt里面的内容，表示网络下载中，文件字节因为网络波动等问题产生缺失，会看到不同的结果
+        // 0.判断data-download是否存在：
+        File download = new File("Java-IOStream/src/main/resources/DigestStream/data-download.txt");
+        if (!download.exists()){
+            System.out.println("不存在下载文件data-download.txt，请先运行DigestOutputStreamDemo");
+            return;
+        }
+        // 1.先打印原始文件内容和它对应的Hash字节
+        System.out.println("===================== 原始文件的内容及Hash值 =====================");
         InputStream dataStream = DigestInputStreamDemo.class.getResourceAsStream("/data.txt");
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         DigestInputStream digestInputStream = new DigestInputStream(dataStream, md5);
         int available = digestInputStream.available();
         byte[] bytes = new byte[available];
         int read = digestInputStream.read(bytes);
+        // 读入原始内容的Hash值
+        InputStream dataDigest = DigestInputStreamDemo.class.getResourceAsStream("/DigestStream/data-digest.txt");
+        byte[] dataDigestBytes = new byte[dataDigest.available()];
+        dataDigest.read(dataDigestBytes);
         if (read == available){
+            System.out.println("data.txt内容(字节)：" + Arrays.toString(bytes));
+            System.out.println("data.txt内容(文本)：" + new String(bytes));
+            System.out.println();
+
             byte[] digest = digestInputStream.getMessageDigest().digest();
-            System.out.println("MD5（字节）：" + Arrays.toString(digest));
+            System.out.println("MD5Hash（字节）：" + Arrays.toString(digest));
+            System.out.println("MD5Hash（字符，可能会乱码）:" + new String(digest, StandardCharsets.US_ASCII));
             System.out.println();
-            System.out.println("MD5（字符，可能会乱码）:" + new String(digest, StandardCharsets.US_ASCII));
-            System.out.println();
-            System.out.println("===================== 校验文件前后 =========================");
-            InputStream dataDigest = DigestInputStreamDemo.class.getResourceAsStream("/data-digest.txt");
-            byte[] dataDigestBytes = new byte[dataDigest.available()];
-            dataDigest.read(dataDigestBytes);
-            System.out.println("生成文件时产生的Hash字节：" + Arrays.toString(dataDigestBytes));
-            System.out.println("输入流读入文件时产生的Hash字节" + Arrays.toString(digest));
-            System.out.println("两个字节集是否相等：" + Arrays.equals(dataDigestBytes, digest));
-            if (Arrays.equals(dataDigestBytes, digest))
-                System.out.println("可以看出当前后文件内容相同的时候，生成的MD5Hash值也相同！");
-            else
-                System.out.println("当因为网络环境等原因，导致文件出现缺失，两个文件生成的MD5Hash值就会不一样！因此这个时候就会知道下载的文件不全");
+
+            System.out.println("data-digest.txt：" + Arrays.toString(dataDigestBytes));
             dataDigest.close();
         }
         digestInputStream.close();
         dataStream.close();
+        System.out.println("============================================================");
+        System.out.println();
+        // 2.读入下载的文件，并开始校验文件完整性
+        InputStream downloadFile = DigestInputStreamDemo.class.getResourceAsStream("/DigestStream/data-download.txt");
+        DigestInputStream downloadFileDigest = new DigestInputStream(downloadFile, md5);
+        byte[] bytes1 = new byte[downloadFile.available()];
+        int read1 = downloadFileDigest.read(bytes1);
+        if (read1 == bytes1.length){
+            System.out.println("data-download.txt的内容（字节）：" + Arrays.toString(bytes1));
+            System.out.println("data-download.txt的内容（字符）：" + new String(bytes1));
+            System.out.println();
+            byte[] digest = downloadFileDigest.getMessageDigest().digest();
+            System.out.println("data-download.txt的Hash值（字节）：" + Arrays.toString(digest));
+            System.out.println("data-download.txt的Hash值（字符）：" + new String(digest));
+            System.out.println();
+            System.out.println("data.txt和data-download.txt是否相同：" + Arrays.equals(dataDigestBytes, digest));
+            if (Arrays.equals(dataDigestBytes, digest))
+                System.out.println("可以看出当前后文件内容相同的时候，生成的MD5Hash值也相同！");
+            else
+                System.out.println("当因为网络环境等原因，导致文件出现缺失，两个文件生成的MD5Hash值就会不一样！因此这个时候就会知道下载的文件不全");
+        }
+        downloadFileDigest.close();
+        downloadFile.close();
     }
 }
--- src/main/java/cn/argentoaskia/demo/DigestOutputStreamDemo.java
+++ src/main/java/cn/argentoaskia/demo/DigestOutputStreamDemo.java
@@ -3,10 +3,12 @@ package cn.argentoaskia.demo;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.security.DigestOutputStream;
 import java.security.MessageDigest;
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.Function;
 
 /**
  * DigestOutputStream可以实现在创建内容或者文件的时候，生成该文件的对应Hash值，这样当对方下载完文件的时候，通过DigestInputStream
@@ -15,29 +17,31 @@ import java.util.Arrays;
  */
 public class DigestOutputStreamDemo {
     public static void main(String[] args) throws Exception {
-        // 写出内容的Hash值放在data-digest.txt
-        File output = new File("Java-IOStream/src/main/resources/data-digest.txt");
+        System.out.println("============ 下面模拟文件打包时生成MD5Hash的过程 ============");
+        // 1.写出内容的Hash值放在data-digest.txt
+        File output = new File("Java-IOStream/src/main/resources/DigestStream/data-digest.txt");
         if (!output.exists()){
             output.createNewFile();
         }
         FileOutputStream fileOutputStream = new FileOutputStream(output);
 
-        // 读入内容
+        // 2.读入内容
         InputStream data = DigestOutputStreamDemo.class.getResourceAsStream("/data.txt");
         int available = data.available();
         byte[] dataBuf = new byte[available];
         data.read(dataBuf);
-        data.close();
 
-        // 创建md5算法实例，并写出data.txt的内容到控制台
+        // 3.创建md5算法实例，并写出data.txt的内容到控制台
         MessageDigest md5 = MessageDigest.getInstance("md5");
         System.out.println("data.txt中的内容：");
         DigestOutputStream digestOutputStream = new DigestOutputStream(System.out, md5);
         digestOutputStream.write(dataBuf);
+        // 冲刷输出流
+        digestOutputStream.flush();
         System.out.println();
         System.out.println();
 
-        // 获取摘密Hash值,并写到文件中
+        // 4.获取摘密Hash值,并写到文件中
         byte[] digest = digestOutputStream.getMessageDigest().digest();
         System.out.println("md5值（字节）：");
         System.out.println(Arrays.toString(digest));
@@ -45,10 +49,51 @@ public class DigestOutputStreamDemo {
         System.out.println("md5值（字符，可能会显示乱码）：");
         System.out.println(new String(digest));
         fileOutputStream.write(digest);
+        // 冲刷输出流
+        digestOutputStream.flush();
 
-        // 关闭摘密流和数据输出流
+        System.out.println();
+        System.out.println("==================== 华丽丽 ===================");
+        System.out.println();
+        System.out.println("==================== 下面模拟文件下载过程 ====================");
+        System.out.println("1.文件下载完成之后会在resources/DigestStream下面创建一个data-download.txt！");
+        System.out.println("2.根据系统随机来判定创建的文件是否和源文件data.txt相同，以模拟下载过程中的网络传输的不稳定性！");
+        Random random = new Random();
+        File downloadFile = new File("Java-IOStream/src/main/resources/DigestStream/data-download.txt");
+        if (!downloadFile.exists()){
+            downloadFile.createNewFile();
+        }
+        FileOutputStream downloadFileStream = new FileOutputStream(downloadFile);
+        if (random.nextBoolean()){
+            // 创建相同的文件
+            downloadFileStream.write(dataBuf);
+            // 冲刷输出流
+            digestOutputStream.flush();
+        }else{
+            // 创建不同的文件
+            // 字符数组转包装器数组utils
+            List<Byte> byteList = new ArrayList<>();
+            for (byte b :
+                    dataBuf) {
+                byteList.add(b);
+            }
+            // 去掉其中几个字节，代表网络下载时部分字节下载不回来或者网络卡顿导致延迟等
+            for (int i = 0; i < random.nextInt(byteList.size() / 2); i++) {
+                byteList.remove(random.nextInt(byteList.size()));
+            }
+            byte[] bytes = new byte[byteList.size()];
+            int i = 0;
+            for (Byte b : byteList) bytes[i++] = b;
+            downloadFileStream.write(bytes);
+            // 冲刷输出流
+            digestOutputStream.flush();
+        }
+        System.out.println("文件下载完成，后期请运行DigestInputStreamDemo来查看文件是否完整！！");
+        // 切记流的关闭要放在最后，因为文中第三步会导致System.out关闭，也就无法输出任何内容到控制台
+        data.close();
         fileOutputStream.close();
         digestOutputStream.close();
+        downloadFileStream.close();
 
     }
 }
